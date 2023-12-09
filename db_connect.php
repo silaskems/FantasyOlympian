be35c0cb1c84a8270ba8f7be02c1be281cc578a7
silaskems <144051236+silaskems@users.noreply.github.com>
2023-12-09 11:24:37 -0600

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root"; // default XAMPP username
 $password = ""; // default XAMPP password is empty
-$dbname = "fodb"; // replace with your database name
+$dbname = "fodb"; 
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -11,5 +11,4 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
-?>
+// echo "Connected successfully"; 
